8d83413e2e88be316dba36d3a28aa149b996e606
mariagracelinaa <gracezhouu@gmail.com>
2022-02-18 19:21:28 +0700

    init MainFragment code

--- app/src/main/java/com/ubaya/s160419024_week2/MainActivity.kt
+++ app/src/main/java/com/ubaya/s160419024_week2/MainActivity.kt
@@ -2,6 +2,7 @@ package com.ubaya.s160419024_week2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.fragment_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
